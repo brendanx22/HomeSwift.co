e9fce7471a1b375d1f988cd39b8115a3d92e475b
Brendan Nwanze <140804253+brendanx22@users.noreply.github.com>
2025-12-01 02:30:35 +0100

    fix

--- src/pages/ListPropertyForm.jsx
+++ src/pages/ListPropertyForm.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import PropertyFeatures from './PropertyFeatures';
 import PropertyMap from '../components/PropertyMap';
 import { PropertyAPI } from '../lib/propertyAPI';
